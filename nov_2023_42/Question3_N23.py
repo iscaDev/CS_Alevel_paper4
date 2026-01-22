643de9443ea979815b1540661bb276e67e25555c
Isfandiyor Mullajonov <114288417+iscaDev@users.noreply.github.com>
2026-01-22 12:08:43 +0500

    Add files via upload

---
+++ nov_2023_42/Question3_N23.py
@@ -0,0 +1,6 @@
+class Character:
+    def __init__(self, name, birth, intelligenceP, speedP):
+        self.__CharacterName = name # CharacterName as STRING
+        self.__DateOfBirth = birth # DateOfBirth as DATE
+        self.__Intelligence = intelligenceP # Intelligence as REAL
+        self.__Speed = speedP # Speed as INTEGER
