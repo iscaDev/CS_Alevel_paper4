c326fbe7fd66889eb7deb16eb1a862539eba6052
Isfandiyor Mullajonov <114288417+iscaDev@users.noreply.github.com>
2026-04-29 15:21:25 +0500

    Add files via upload

--- nov_2024_41/Question2_N24.py
+++ nov_2024_41/Question2_N24.py
@@ -1,31 +1,36 @@
 class Horse:
-    def __init__(self, nameP, MaxHeightP, SuccessP):
-        self.__Name = nameP # Name as STRING
-        self.__MaxFenceHeight = MaxHeightP # MaxFenceHeight as INTEGER
-        self.__PercentageSuccess = SuccessP # PercentageSuccess as INTEGER
+    def __init__(self, nameP, heightP, successP):
+        self.__Name = nameP # Name as String
+        self.__MaxFenceHeight = heightP # MaxFenceHeight as Integer
+        self.__PercentageSuccess = successP # PercentageSuccess as Integer
+
     def GetName(self):
         return self.__Name
     def GetMaxFenceHeight(self):
         return self.__MaxFenceHeight
-    def Success(self, heightP, riskP):
-        if heightP > self.__MaxFenceHeight:
-            return self.__PercentageSuccess * 0.20
+
+    def Success(self, heightF, riskF):
+        PercentageChance = 0
+        if heightF > self.__MaxFenceHeight:
+            PercentageChance = 0.20*self.__PercentageSuccess
         else:
-            if riskP == 1:
-                return self.__PercentageSuccess
-            elif riskP == 2:
-                return self.__PercentageSuccess * 0.9
-            elif riskP == 3:
-                return self.__PercentageSuccess * 0.8
-            elif riskP == 4:
-                return self.__PercentageSuccess * 0.7
+            if riskF == 1:
+                PercentageChance = self.__PercentageSuccess
+            elif riskF == 2:
+                PercentageChance = 0.9*self.__PercentageSuccess
+            elif riskF == 3:
+                PercentageChance = 0.8*self.__PercentageSuccess
+            elif riskF == 4:
+                PercentageChance = 0.7*self.__PercentageSuccess
             else:
-                return self.__PercentageSuccess * 0.6
+                PercentageChance = 0.6*self.__PercentageSuccess
+        return PercentageChance
 
 class Fence:
     def __init__(self, heightP, riskP):
-        self.__Height = heightP # Height as INTEGER
-        self.__Risk = riskP # Risk as INTEGER
+        self.__Height = heightP # Height as Integer
+        self.__Risk = riskP # Risk as Integer
+
     def GetHeight(self):
         return self.__Height
     def GetRisk(self):
@@ -33,21 +38,40 @@ class Fence:
 
 
 # main program
-Horses = [] # Array of type Horse
-horse1 = Horse("Beauty", 150, 72)
-horse2 = Horse("Jet", 160, 65)
-Horses.append(horse1)
-Horses.append(horse2)
+Horses = []
+Horses.append(Horse("Beauty", 150, 72))
+Horses.append(Horse("Jet", 160, 65))
 print(Horses[0].GetName())
 print(Horses[1].GetName())
 
-Course = [] # Array of type Fence
+Course = []
 for i in range(4):
-    height = int(input("Please enter the height of the fence: "))
-    while height < 70 or height > 180:
-        height = int(input("Please enter valid height value: "))
-    risk = int(input("Please enter the risk of the fence: "))
-    while risk < 1 or risk > 5:
-        risk = int(input("Please enter the valid risk value: "))
+    height = int(input(f"Pleas enter the height of the fence {i+1}: "))
+    while height <70 or height>180:
+        height = input(f"Pleas enter the height of the fence {i+1}: ")
+    risk = int(input(f"Pleas enter the risk of the fence {i+1}: "))
+    while risk<1 or risk>5:
+        risk1 = int(input(f"Pleas enter the risk of the fence {i+1}: "))
     Course.append(Fence(height, risk))
 
+for j in range(4):
+    horse1 = f"The horse {Horses[0].GetName()} at fence {j+1} has a {Horses[0].Success(Course[j].GetHeight(), Course[j].GetRisk())} % chance of success"
+    print(horse1)
+for k in range(4):
+    horse2 = f"The horse {Horses[1].GetName()} at fence {k+1} has a {Horses[1].Success(Course[k].GetHeight(), Course[k].GetRisk())} % chance of success"
+    print(horse2)
+
+total1 = 0
+for _ in range(4):
+    total1 += (Horses[0].Success(Course[_].GetHeight(), Course[_].GetRisk()))
+average1 = total1/4
+print(f"The horse {Horses[0].GetName()} has an average {average1} chance of jumping over all four fences")
+total2 = 0
+for l in range(4):
+    total2 += (Horses[1].Success(Course[l].GetHeight(), Course[l].GetRisk()))
+average2 = total2/4
+print(f"The horse {Horses[1].GetName()} has an average {average2} chance of jumping over all four fences")
+if average1>average2:
+    print(f"The {Horses[0].GetName()} has the highest average chance of success")
+else:
+    print(f"The {Horses[1].GetName()} has the highest average chance of success")
